04d2d4ca69d0246edd1c8d84d47d4c8e28599616
locovna <locovna@gmail.com>
2017-02-20 22:53:20 +0200

    create class WeatherFetch

---
+++ wiiiter/app/src/main/java/com/locovna/wiiiter/util/WeatherFetch.java
@@ -0,0 +1,11 @@
+package com.locovna.wiiiter.util;
+
+import java.io.IOException;
+
+public class WeatherFetch {
+
+  public String getUrlString(String urlSpec) throws IOException {
+    return  urlSpec;
+  }
+
+}
