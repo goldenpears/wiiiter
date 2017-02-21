efc364ba933e80da37ebc7c2bb5d7dd98f43d1fb
locovna <locovna@gmail.com>
2017-02-21 23:08:46 +0200

    add gson dependency

--- wiiiter/app/src/main/java/com/locovna/wiiiter/util/WeatherFetch.java
+++ wiiiter/app/src/main/java/com/locovna/wiiiter/util/WeatherFetch.java
@@ -1,11 +1,5 @@
 package com.locovna.wiiiter.util;
 
-import java.io.IOException;
-
 public class WeatherFetch {
-
-  public String getUrlString(String urlSpec) throws IOException {
-    return  urlSpec;
-  }
 
 }
